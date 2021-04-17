834fb6b36b297c88a36e4a8b0a69016d21674ecd
Shreyaskari2007237 <78490664+Shreyaskari2007237@users.noreply.github.com>
2021-04-17 20:13:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+
+var bob1,bob2,bob3,bob4,bob5;
+var roof;
+var rope1,rope2,rope3,rope4,rope5;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	roof=new Roof(width/2,height/4,600,50);
+	
+	bob1=new Bob(230,400,80);
+	bob2=new Bob(310,400,80);
+	bob3=new Bob(390,400,80);
+	bob4=new Bob(470,400,80);
+	bob5=new Bob(550,400,80);
+	
+  rope1=new Rope(bob1.body,roof.body,-160,0);
+	rope2=new Rope(bob2.body,roof.body,-80,0);
+	rope3=new Rope(bob3.body,roof.body,0,0);
+	rope4=new Rope(bob4.body,roof.body,80,0);
+	rope5=new Rope(bob5.body,roof.body,160,0);
+
+
+  Engine.run(engine);
+	
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("skyblue");
+  Engine.update(engine);
+  
+  roof.display();
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+  
+ 
+}
+
+
+
